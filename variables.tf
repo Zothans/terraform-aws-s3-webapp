4b5bc691afd3bb31a5965894217d1acd69596217
Zothans <zothan.sanga@hashicorp.com>
2022-08-18 11:07:40 +1000

    123

--- variables.tf
+++ variables.tf
@@ -1,6 +1,14 @@
-variable "region" {}
-variable "prefix" {}
-variable "name" {yes}
+variable "region" {
+description = "This is the cloud hosting region where your webapp will be deployed."
+}
+
+variable "prefix" {
+ description = "This is the environment your webapp will be prefixed with. dev, qa, or prod"
+}
+
+variable "name" {
+description = "Your name to attach to the webapp address"
+}
 
 variable "testvar" {
   type = string
