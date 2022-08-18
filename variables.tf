d351d8dac051ad26716fe190c85c4191e90b889d
Zothans <105400251+Zothans@users.noreply.github.com>
2022-08-18 10:20:39 +1000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
 variable "region" {}
 variable "prefix" {}
 variable "name" {yes}
+
+variable "testvar" {
+  type = string
+  default = ""
+  description = "This is a test variable where default is empty"
+}
+
+variable "testvar_default" {
+  type = string
+  default = null
+  description = "This is a test variable set it null"
+}
